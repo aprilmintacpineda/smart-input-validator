c0b1bdf801a018da072f786beb6333d051dfacfe
April Mintac Pineda <aprilmintacpineda@gmail.com>
2018-01-29 16:40:14 +0800

    used typeof

--- src/index.js
+++ src/index.js
@@ -15,8 +15,8 @@ export default function validator(inputs, rules, customMesssages = null) {
       let validationResult = null;
       let cb_params = [];
 
-      if (!validationRules[rule]) throw new Error(`input-validator-js: unknown rule \`${rule}\` provided for field \`${field}\`. Please refer to the docs for more info.`);
-      if (!inputs[field]) throw new Error(`input-validator-js: unknown field ${field} in inputs.`);
+      if (typeof !validationRules[rule] == 'undefined') throw new Error(`input-validator-js: unknown rule \`${rule}\` provided for field \`${field}\`. Please refer to the docs for more info.`);
+      if (typeof inputs[field] == 'undefined') throw new Error(`input-validator-js: unknown field ${field} in inputs.`);
 
       // :value
       if (val) {

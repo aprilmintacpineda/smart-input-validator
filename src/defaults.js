afad73678b880d5e1550b1e5f9e1ce33f04a48a8
April Mintac Pineda <aprilmintacpineda@gmail.com>
2018-01-30 12:58:31 +0800

    len,notRegex,regex;

--- src/defaults.js
+++ src/defaults.js
@@ -1,9 +1,14 @@
 export const errMessages = {
-  required: fieldName => `${fieldName} is required.`,
-  equals: (field, subject) => `${field} does not match with ${subject}.`,
+  // length related validation messages,
   min: (field, len) => `${field} must be at least ${len} character${len > 1? 's' : ''} long.`,
   max: (field, len) => `${field} must be at least ${len} character${len > 1? 's' : ''} long.`,
   between: (field, min, max) => `${field} must be at least ${min} to ${max} characters long.`,
+  len: (field, len) => `${field} must be ${len} character${len > 1? 's' : ''} long.`,
+  // value related validation messages
+  required: fieldName => `${fieldName} is required.`,
+  equals: (field, subject) => `${field} does not match with ${subject}.`,
   email: () => `Invalid email.`,
-  allowedChars: field => `${field} has invalid characters.`
+  allowedChars: field => `${field} has invalid characters.`,
+  regex: field => `${field} has invalid characters.`,
+  notRegex: field => `${field} has invalid characters.`
 };

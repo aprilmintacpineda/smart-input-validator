c8d4d07e0673ed5b7cf2b9bee8a9ab8a2f1e91b7
April Mintac Pineda <aprilmintacpineda@gmail.com>
2018-02-28 14:55:54 +0800

    used for-loop instead of for-of;

--- src/index.js
+++ src/index.js
@@ -15,8 +15,11 @@ export default function validator(inputs, rules, customMesssages = null) {
 
   Object.keys(newRules).forEach(field => {
     const targetField = field.replace(/\_$/, '');
+    const segments = rules[targetField].split('|');
 
-    for (let segment of rules[targetField].split('|')) {
+    for (let a = 0; a <= segments.length - 1; a++) {
+      const segment = segments[a];
+
       // rule:value
       const [rule, val] = segment.split(':');
       let validationResult = null;

48157cdb27093835615a4d3f4bb07db479141cc8
April Mintac Pineda <aprilmintacpineda@gmail.com>
2018-11-02 14:44:26 +0800

    updated dependencies; updated build settings;

--- build.js
+++ build.js
@@ -8,12 +8,14 @@ const source = fs.readFileSync(path.join(__dirname, '/src/index.js'), 'utf8');
 const code = babelCore.transform(source, {
   babelrc: false,
   presets: ['@babel/preset-env'],
-  plugins: ['@babel/plugin-syntax-object-rest-spread']
+  plugins: ['@babel/plugin-syntax-object-rest-spread'],
+  comments: false
 }).code;
 const codeMin = babelCore.transform(source, {
   babelrc: false,
   presets: ['@babel/preset-env', 'minify'],
-  plugins: ['@babel/plugin-syntax-object-rest-spread']
+  plugins: ['@babel/plugin-syntax-object-rest-spread'],
+  comments: false
 }).code;
 
 fs.writeFileSync(path.join(__dirname, '/lib/index.js'), code, 'utf8');
